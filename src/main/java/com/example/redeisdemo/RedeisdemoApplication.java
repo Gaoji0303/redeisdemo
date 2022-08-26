bde544ad0d4a0fbc0cb1b55ff7fed7e2d341970c
gaoji <git@github.com:Gaoji0303/demo.git>
2022-08-27 00:21:57 +0800

    redisDemo

---
+++ src/main/java/com/example/redeisdemo/RedeisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.redeisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeisdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedeisdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/redeisdemo/redis/redisTest.java
@@ -0,0 +1,17 @@
+//package com.example.redeisdemo.redis;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+//public class redisTest {
+//    @Autowired
+//    RedisTemplate redisTemplate;
+//
+//    public static void main(String[] args) {
+//
+//        redisTemplate.opsForValue().set("num","123");
+//    }
+//
+//
+//
+//}
